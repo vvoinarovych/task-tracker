670023346b25a315c22aa1d939954402a9db5a33
Valerii <87275444+vvoinarovych@users.noreply.github.com>
2024-07-21 00:34:17 +0200

    refactor: cleaned imports

--- task-tracker-backend/src/main/java/com/vvoinarovych/tasktrackerbackend/security/controller/AuthController.java
+++ task-tracker-backend/src/main/java/com/vvoinarovych/tasktrackerbackend/security/controller/AuthController.java
@@ -1,26 +1,14 @@
 package com.vvoinarovych.tasktrackerbackend.security.controller;
 
-import com.vvoinarovych.tasktrackerbackend.dto.*;
-import com.vvoinarovych.tasktrackerbackend.model.User;
-import com.vvoinarovych.tasktrackerbackend.repository.UserRepository;
-import com.vvoinarovych.tasktrackerbackend.security.jwt.JwtUtils;
+import com.vvoinarovych.tasktrackerbackend.dto.JwtResponse;
+import com.vvoinarovych.tasktrackerbackend.dto.LoginRequest;
+import com.vvoinarovych.tasktrackerbackend.dto.MessageResponse;
+import com.vvoinarovych.tasktrackerbackend.dto.SignupRequest;
 import com.vvoinarovych.tasktrackerbackend.security.services.AuthService;
-import com.vvoinarovych.tasktrackerbackend.security.services.UserDetailsImpl;
-import io.jsonwebtoken.Jwt;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -28,9 +16,6 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
-    private final AuthenticationManager authenticationManager;
-
-    private final JwtUtils jwtUtils;
 
     private final AuthService authService;
 
