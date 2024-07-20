78fdea06ed1b471d0b955ce19f2c77d8427a7f71
Valerii <87275444+vvoinarovych@users.noreply.github.com>
2024-07-21 00:33:05 +0200

    feat: create exception handler

---
+++ task-tracker-backend/src/main/java/com/vvoinarovych/tasktrackerbackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.vvoinarovych.tasktrackerbackend.exception;
+
+import com.vvoinarovych.tasktrackerbackend.dto.ExceptionResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleException(UserExistsException exp) {
+        return ResponseEntity
+                .status(CONFLICT)
+                .body(ExceptionResponse.builder()
+                        .error(exp.getMessage())
+                        .build()
+                );
+    }
+}
