17f96fa28856dc3852cb882badd80788b90a9d86
Valerii <87275444+vvoinarovych@users.noreply.github.com>
2024-07-21 00:30:54 +0200

    feat: create Roles and User repos

---
+++ task-tracker-backend/src/main/java/com/vvoinarovych/tasktrackerbackend/model/Role.java
@@ -0,0 +1,23 @@
+package com.vvoinarovych.tasktrackerbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "tab_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+}
