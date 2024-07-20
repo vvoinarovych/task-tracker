5ab3eba36b9ca806ec0349dc1ead727af8045411
Valerii <87275444+vvoinarovych@users.noreply.github.com>
2024-07-21 00:21:32 +0200

    refactor: rename init sql file

--- task-tracker-backend/src/main/resources/demo-data.sql
+++ task-tracker-backend/src/main/resources/demo-data.sql
@@ -1,3 +1,2 @@
 INSERT INTO task_tracker.tab_role (name) VALUES('ROLE_USER');
-INSERT INTO task_tracker.tab_role (name) VALUES('ROLE_MODERATOR');
 INSERT INTO task_tracker.tab_role (name) VALUES('ROLE_ADMIN');
