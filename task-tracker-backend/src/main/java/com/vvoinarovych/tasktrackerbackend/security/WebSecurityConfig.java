a55910e5d59985d67a457d7101f233837c513f2f
Valerii <87275444+vvoinarovych@users.noreply.github.com>
2024-07-22 18:29:14 +0200

    feat: add swagger urls to security config

--- task-tracker-backend/src/main/java/com/vvoinarovych/tasktrackerbackend/security/WebSecurityConfig.java
+++ task-tracker-backend/src/main/java/com/vvoinarovych/tasktrackerbackend/security/WebSecurityConfig.java
@@ -62,7 +62,16 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers(
                                         "/auth/**",
-                                        "/tasks/**")
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html")
                                 .permitAll()
                                 .anyRequest().authenticated()
                 );
